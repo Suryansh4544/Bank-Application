9ac0c34cd3976b7688ad59b64b8ff5e43ad7cf52
Suryansh4544 <suryansh4544@gmail.com>
2022-04-21 15:17:49 +0530

    Main package changed

--- src/main/java/com/bank/demo/BankApplication.java
+++ src/main/java/com/bank/demo/BankApplication.java
@@ -1,3 +1,4 @@
+package com.bank.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
